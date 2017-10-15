03b0766f98e6a2ff872303f94e8d3aaf83663085
penguin138 <fridaythethirteens@gmail.com>
2017-10-15 06:02:12 +0200

    add first firebase support

--- server/backend.py
+++ server/backend.py
@@ -3,6 +3,7 @@ import json
 import pymongo
 import argparse
 import os
+from pyfcm import FCMNotification
 
 
 parser = argparse.ArgumentParser()
@@ -16,18 +17,31 @@ print('DEBUG: %d' % args.debug)
 app = Flask(NAME, static_url_path=os.path.dirname(os.getcwd()))
 cli = pymongo.MongoClient('localhost', 27017)
 db = cli[NAME]
+push_service = FCMNotification(api_key=os.getenv("API_KEY"))
 
 
 @app.route("/api", methods=['GET', 'POST'])
 def api():
-	try:
-		r = json.loads(request.data)
-		for hyp in r['hypotheses']:
-			number = hyp['number']
-			db.numbers.update({'_id': number}, {'longitude': float(r['longitude']), 'latitude': float(r['latitude']), 'confidence': float(hyp['confidence']), 'photo': hyp['photo']}, upsert=True)
-	except:
-		return "Format mismatch"
-	return "ok"
+    message_title = "Your car was found!"
+    try:
+        r = json.loads(request.data)
+        for hyp in r['hypotheses']:
+            number = hyp['number']
+            db.numbers.update({'_id': number},
+                              {'longitude': float(r['longitude']),
+                               'latitude': float(r['latitude']),
+                               'confidence': float(hyp['confidence']),
+                               'photo': hyp['photo']}, upsert=True)
+            tokens = db.subscriptions.find_one('number', number)
+            for token in tokens:
+                data_message = {"EXTRA_LATITUDE": float(r['latitude'],
+                                "EXTRA_LONGITUDE": float(r['longitude']}
+                result = push_service.notify_single_device(registration_id=token,
+                message_title=message_title, data_message=data_message,
+                click_action="MapsActivity")
+    except:
+        return "Format mismatch"
+    return "ok"
 
 
 @app.route("/debug", methods=['GET', 'POST'])
@@ -58,6 +72,20 @@ def query():
 	return json.dumps(result)
 
 
+@app.route("/subscribe", methods=["POST"])
+def subscribe():
+    data = json.loads(request.data)
+    tokens = db.subscriptions.find_one('number', data['number'])
+    if tokens is None:
+        db.subscriptions.update({data['number']: [data['token']]})
+    else:
+        tokens.append(data['token'])
+        db.subscriptions.update({data['number']: tokens})
+    return {"status": True}
+
+
+
+
 @app.route('/')
 def serve_static():
 	return app.send_static_file('index.html')
@@ -80,5 +108,3 @@ def mainjs():
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=12345)
-
-

caf32abe18beb1949f4a210ad3bf5e833b426dba
penguin138 <fridaythethirteens@gmail.com>
2017-10-15 07:45:37 +0300

    remove silent exceptions

--- server/backend.py
+++ server/backend.py
@@ -23,25 +23,22 @@ push_service = FCMNotification(api_key=os.getenv("API_KEY"))
 @app.route("/api", methods=['GET', 'POST'])
 def api():
     message_title = "Your car was found!"
-    try:
-        r = json.loads(request.data)
-        for hyp in r['hypotheses']:
-            number = hyp['number']
-            db.numbers.update({'_id': number},
-                              {'longitude': float(r['longitude']),
-                               'latitude': float(r['latitude']),
-                               'confidence': float(hyp['confidence']),
-                               'photo': hyp['photo']}, upsert=True)
-            tokens = db.subscriptions.find_one('number', number)
-            for token in tokens:
-                data_message = {"EXTRA_LATITUDE": float(r['latitude']),
-                                "EXTRA_LONGITUDE": float(r['longitude'])}
-                result = push_service.notify_single_device(registration_id=token,
-                                                           message_title=message_title,
-                                                           data_message=data_message,
-                                                           click_action="MapsActivity")
-    except:
-        return "Format mismatch"
+    r = json.loads(request.data)
+    for hyp in r['hypotheses']:
+        number = hyp['number']
+        db.numbers.update({'_id': number},
+                          {'longitude': float(r['longitude']),
+                           'latitude': float(r['latitude']),
+                           'confidence': float(hyp['confidence']),
+                           'photo': hyp['photo']}, upsert=True)
+        tokens = db.subscriptions.find_one('number', number)
+        for token in tokens:
+            data_message = {"EXTRA_LATITUDE": float(r['latitude']),
+                            "EXTRA_LONGITUDE": float(r['longitude'])}
+            result = push_service.notify_single_device(registration_id=token,
+                                                       message_title=message_title,
+                                                       data_message=data_message,
+                                                       click_action="MapsActivity")
     return "ok"
 
 
@@ -106,4 +103,4 @@ def mainjs():
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=12345)
+    app.run('0.0.0.0', port=12346)
